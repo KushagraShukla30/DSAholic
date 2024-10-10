126cbafa7cf4ca20055e4510d639a9370b46e5d1
Ash4-k <ashlesh.kanchan@gmail.com>
2024-10-10 07:23:43 +0530

    Merge Edit

--- test1.c
+++ test1.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 int main(){
-<<<<<<< HEAD
     printf("Hello world! 2 3 4 6.local");
-=======
+
     printf("Hello world! 2 3 4 5.global");
->>>>>>> 6b732cbf2159c1155c8c8874b3e2d0311d8e9bf3
 }
